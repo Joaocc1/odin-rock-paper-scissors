a5d580af9c5a9f89859f29e9d59f9407052c8b6a
Joaoc <joaoc86@gmail.com>
2025-11-15 15:46:24 +0000

    expand to generate an actual string from the computer choice function

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ console.log("Hello World");
 
 // write variable that holds computer random choice
 function getComputerChoice() {
-  return Math.round(Math.random() * 2) + 1;
+  let value = Math.round(Math.random() * 2) + 1;
+
+  console.log(value);
+
+  if (value === 1) {
+    return "Rock";
+  } else if (value === 2) {
+    return "Paper";
+  } else if (value === 3) {
+    return "Scissors";
+  }
 }
 
 console.log(getComputerChoice());
-
 // ask user for a choice and save it
 // show both choices
 // compare choices and return a winner
