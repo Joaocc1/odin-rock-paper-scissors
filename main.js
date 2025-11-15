d5aca3c7703f33e75c183608ea43f9ed5f9b84cb
Joaoc <joaoc86@gmail.com>
2025-11-15 15:33:16 +0000

    add function to generate computer choice

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+// write variable that holds computer random choice
+function getComputerChoice() {
+  return Math.round(Math.random() * 2) + 1;
+}
+
+console.log(getComputerChoice());
+
+// ask user for a choice and save it
+// show both choices
+// compare choices and return a winner
