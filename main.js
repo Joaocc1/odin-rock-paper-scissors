213caf3cac8397ed334ebe80db968816954be2c3
Joaoc <joaoc86@gmail.com>
2025-11-16 14:53:58 +0000

    init function to play 5 rounds

--- main.js
+++ main.js
@@ -27,35 +27,42 @@ function getHumanChoice() {
 
 // show both choices
 // compare choices and return a winner
-function playRound(humanChoice, computerChoice) {
-  // human wins
-  if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    roundOutcome = "win";
-    winHand = humanChoice;
-    loseHand = computerChoice;
-    humanScore++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
 
-    // computer wins
-  } else if (
-    (computerChoice === "rock" && humanChoice === "scissors") ||
-    (computerChoice === "paper" && humanChoice === "rock") ||
-    (computerChoice === "scissors" && humanChoice === "paper")
-  ) {
-    roundOutcome = "lose";
-    winHand = computerChoice;
-    loseHand = humanChoice;
-    computerScore++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-  } else {
-    console.log("You draw!");
+function playGame(humanSelection, computerSelection) {
+  for (let i = 0; i < 5; i++) {
+    playRound(humanSelection, computerSelection);
   }
 
-  console.log(`You: ${humanScore}; Computer: ${computerScore}`);
+  function playRound(humanChoice, computerChoice) {
+    // human wins
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      roundOutcome = "win";
+      winHand = humanChoice;
+      loseHand = computerChoice;
+      humanScore++;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+
+      // computer wins
+    } else if (
+      (computerChoice === "rock" && humanChoice === "scissors") ||
+      (computerChoice === "paper" && humanChoice === "rock") ||
+      (computerChoice === "scissors" && humanChoice === "paper")
+    ) {
+      roundOutcome = "lose";
+      winHand = computerChoice;
+      loseHand = humanChoice;
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    } else {
+      console.log("You draw!");
+    }
+
+    console.log(`You: ${humanScore}; Computer: ${computerScore}`);
+  }
 }
 
-playRound(humanSelection, computerSelection);
+playGame(humanSelection, computerSelection);
